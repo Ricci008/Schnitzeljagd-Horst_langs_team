9c660a0eaaf8d2da0760f7e7db852fe44d52adf9
utigernils <utigernils@gmail.com>
2025-06-03 10:50:47 +0200

    feat; setup tabs with the correct names and routes

--- Schnitzeljagd/src/app/home/home.page.ts
+++ Schnitzeljagd/src/app/home/home.page.ts
@@ -3,13 +3,11 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
-export class Tab2Page {
-
+export class HomePage {
   constructor() {}
-
 }
