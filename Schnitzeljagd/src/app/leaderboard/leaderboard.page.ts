96427378a040533bda34082b589c003921db1f9b
Riccardo Greco <Riccardo_Greco@sluz.ch>
2025-06-03 16:03:07 +0200

    feat; added leaderboard with test data

--- Schnitzeljagd/src/app/leaderboard/leaderboard.page.ts
+++ Schnitzeljagd/src/app/leaderboard/leaderboard.page.ts
@@ -1,15 +1,45 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonList,
+  IonLabel,
+  IonItem
+} from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: 'leaderboard.html',
   styleUrls: ['leaderbaord.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton, IonList, IonLabel, IonItem]
 })
 export class LeaderboardPage {
+  mode: 'top' | 'latest' = 'top';
 
+  topList = [
+    { name: 'Max Mustermann', time: 80 },
+    { name: 'Erika Mustermann', time: 90 },
+    { name: 'Hans Müller', time: 100 }
+
+  ];
+
+  latestList = [
+    { name: 'Max Mustermann', time: 30 },
+    { name: 'Erika Mustermann', time: 40 },
+    { name: 'Hans Müller', time: 50 }
+  ];
+
+  get list() {
+    return this.mode === 'top' ? this.topList : this.latestList;
+  }
+
+  setMode(mode: 'top' | 'latest') {
+    this.mode = mode;
+  }
   constructor() {}
 
 }
