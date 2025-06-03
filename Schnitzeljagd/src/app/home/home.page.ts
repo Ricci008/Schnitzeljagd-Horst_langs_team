f7b89950a7572bd41fa9632e3f227fb7065ae9a3
Nico Marcuard <Nico_Marcuard@sluz.ch>
2025-06-03 16:01:14 +0200

    feat: home page created and implemented two modals

--- Schnitzeljagd/src/app/home/home.page.ts
+++ Schnitzeljagd/src/app/home/home.page.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import {IonHeader, IonToolbar, IonTitle, IonContent, IonButton} from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonCard,
+  IonCardHeader, IonCardTitle, IonList, ModalController, IonItem
+} from '@ionic/angular/standalone';
+import {NameInputModalComponent} from "./name-input-modal.component";
+import {PermissionsModalComponent} from "./permissions-modal.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, IonButton],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonList, IonItem],
 })
 export class HomePage {
-  constructor() {}
+
+  constructor(private modalCtrl: ModalController) {}
+
+  async startNewRun() {
+    const nameModal = await this.modalCtrl.create({
+      component: NameInputModalComponent,
+    });
+    await nameModal.present();
+
+    const { data } = await nameModal.onDidDismiss();
+    if (data?.name) {
+      const permissionsModal = await this.modalCtrl.create({
+        component: PermissionsModalComponent,
+      });
+      await permissionsModal.present();
+    }
+  }
 }
