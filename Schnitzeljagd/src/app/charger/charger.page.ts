203d5cdc15ad750206e1a283a48fbd2fd94b1bfd
utigernils <utigernils@gmail.com>
2025-06-03 12:41:33 +0200

    feat; implement progressbar component

--- Schnitzeljagd/src/app/charger/charger.page.ts
+++ Schnitzeljagd/src/app/charger/charger.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {ProgressbarComponent} from "../progressbar/progressbar.component";
 
 @Component({
   selector: 'app-charger',
   templateUrl: './charger.page.html',
   styleUrls: ['./charger.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ProgressbarComponent]
 })
 export class ChargerPage implements OnInit {
 
