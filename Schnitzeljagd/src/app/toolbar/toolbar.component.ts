fcccccab679a4a85a970c2c2941018b6eff1e368
Riccardo Greco <Riccardo_Greco@sluz.ch>
2025-06-03 13:00:14 +0200

    feat; completed toolbar component

--- Schnitzeljagd/src/app/toolbar/toolbar.component.ts
+++ Schnitzeljagd/src/app/toolbar/toolbar.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import {IonIcon, IonTabBar, IonTabButton} from "@ionic/angular/standalone";
+import {addIcons} from "ionicons";
+import {exitOutline, chevronForwardOutline, playSkipForwardOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
+  imports: [
+    IonTabBar,
+    IonTabButton,
+    IonIcon
+  ]
 })
 export class ToolbarComponent  implements OnInit {
+  quest= true;
 
-  constructor() { }
+  constructor() {
+    addIcons({exitOutline, chevronForwardOutline, playSkipForwardOutline});
+  }
 
-  ngOnInit() {}
+  ngOnInit(): void {
+        throw new Error('Method not implemented.');
+    }
 
 }
