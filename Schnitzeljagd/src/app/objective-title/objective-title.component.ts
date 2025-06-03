db29e91537ccc3ad58536bb1fac5203ef9d1aad8
Nico Marcuard <Nico_Marcuard@sluz.ch>
2025-06-03 11:44:08 +0200

    created objective title component

--- Schnitzeljagd/src/app/objective-title/objective-title.component.ts
+++ Schnitzeljagd/src/app/objective-title/objective-title.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {IonCard, IonCardContent, IonCardHeader, IonTitle} from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-objective-title',
   templateUrl: './objective-title.component.html',
   styleUrls: ['./objective-title.component.scss'],
+  imports: [
+    IonCard,
+    IonCardHeader,
+    IonTitle,
+    IonCardContent,
+  ],
+  standalone: true
 })
-export class ObjectiveTitleComponent  implements OnInit {
+export class ObjectiveTitleComponent   {
+  @Input() title: string = '';
+  @Input() description: string = '';
 
   constructor() { }
-
-  ngOnInit() {}
 
 }
