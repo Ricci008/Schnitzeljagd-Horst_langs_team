1c1cb1c86f81e5a541739efa337622f0c100133e
Riccardo Greco <Riccardo_Greco@sluz.ch>
2025-06-04 13:11:39 +0200

    feat; wifi task

--- Schnitzeljagd/src/app/wifi/wifi.page.ts
+++ Schnitzeljagd/src/app/wifi/wifi.page.ts
@@ -8,21 +8,25 @@ import {ProgressbarComponent} from "../progressbar/progressbar.component";
 import {ToolbarComponent} from "../toolbar/toolbar.component";
 import {ScavangerHuntManagerService} from "../services/scavanger-hunt-manager.service";
 
+declare var WifiWizard2: any;
+
 @Component({
   selector: 'app-wifi',
   templateUrl: './wifi.page.html',
   styleUrls: ['./wifi.page.scss'],
   standalone: true,
-    imports: [IonContent, CommonModule, FormsModule, IonFooter, ObjectiveStateComponent, ObjectiveTitleComponent, ProgressbarComponent, ToolbarComponent]
+  imports: [IonContent, CommonModule, FormsModule, IonFooter, ObjectiveStateComponent, ObjectiveTitleComponent, ProgressbarComponent, ToolbarComponent]
 })
 export class WifiPage implements OnInit {
   objectiveNumber: number = 0;
   isTaskDone: boolean = false;
+  private ssidCheckInterval: any;
 
-  constructor(private ScavangerHunt: ScavangerHuntManagerService) { }
+  constructor(private ScavangerHunt: ScavangerHuntManagerService) {}
 
   ngOnInit() {
     this.objectiveNumber = this.ScavangerHunt.getObjectiveNumber() - 1;
+    this.startSSIDCheckLoop();
   }
 
   nextTask() {
@@ -37,10 +41,26 @@ export class WifiPage implements OnInit {
   markTaskDone() {
     this.isTaskDone = true;
     this.ScavangerHunt.endObjective();
-    this.objectiveNumber =+ 1
+    this.objectiveNumber += 1;
   }
 
   exitHunt() {
     this.ScavangerHunt.exitHunt();
+  }
+
+  startSSIDCheckLoop() {
+    this.ssidCheckInterval = setInterval(() => {
+      WifiWizard2.getConnectedSSID(
+        (ssid: string) => {
+          if (ssid === 'ICT-BLJ') {
+            this.markTaskDone();
+            clearInterval(this.ssidCheckInterval);
+          }
+        },
+        (error: any) => {
+          console.error('Error getting connected SSID:', error);
+        }
+      );
+    }, 1000);
   }
 }
