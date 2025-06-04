f77a0034789ee53c777c77316dd5b2cc245951c5
Nico Marcuard <Nico_Marcuard@sluz.ch>
2025-06-04 16:22:07 +0200

    feat: qrcode implement

--- Schnitzeljagd/src/app/qrcode/qrcode.page.ts
+++ Schnitzeljagd/src/app/qrcode/qrcode.page.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonContent, IonFooter, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {IonContent, IonFooter} from '@ionic/angular/standalone';
 import {ObjectiveTitleComponent} from "../objective-title/objective-title.component";
 import {ObjectiveStateComponent} from "../objective-state/objective-state.component";
 import {ProgressbarComponent} from "../progressbar/progressbar.component";
 import {ToolbarComponent} from "../toolbar/toolbar.component";
 import {ScavangerHuntManagerService} from "../services/scavanger-hunt-manager.service";
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+import { BarcodeFormat } from '@zxing/library';
 
 @Component({
   selector: 'app-qrcode',
   templateUrl: './qrcode.page.html',
   styleUrls: ['./qrcode.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, ObjectiveTitleComponent, ObjectiveStateComponent, IonFooter, ProgressbarComponent, ToolbarComponent]
+  imports: [IonContent, ZXingScannerModule, CommonModule, FormsModule, ObjectiveTitleComponent, ObjectiveStateComponent, IonFooter, ProgressbarComponent, ToolbarComponent]
 })
 export class QrcodePage implements OnInit {
   objectiveNumber: number = 0;
   isTaskDone: boolean = false;
 
   constructor(private ScavangerHunt: ScavangerHuntManagerService) { }
+
+  formatsEnabled: BarcodeFormat[] = [BarcodeFormat.QR_CODE];
 
   ngOnInit() {
     this.objectiveNumber = this.ScavangerHunt.getObjectiveNumber() - 1;
@@ -37,10 +41,19 @@ export class QrcodePage implements OnInit {
   markTaskDone() {
     this.isTaskDone = true;
     this.ScavangerHunt.endObjective();
-    this.objectiveNumber =+ 1
+    this.objectiveNumber += 1
   }
 
   exitHunt() {
     this.ScavangerHunt.exitHunt();
   }
+
+  validQRCode = 'M335@ICT-BZ';
+
+  onCodeResult(result: string) {
+    if (result === this.validQRCode) {
+      this.markTaskDone();
+    }
+  }
+
 }
