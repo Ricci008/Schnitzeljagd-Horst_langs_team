4f7bc9ea16e43ee8064b2df512dd89d82e1036b8
utigernils <utigernils@gmail.com>
2025-06-04 11:30:51 +0200

    feat&cleanup; implement permission requesting and cleaning up modals

--- Schnitzeljagd/src/app/home/home.page.ts
+++ Schnitzeljagd/src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import {
   IonHeader,
   IonToolbar,
@@ -6,39 +6,70 @@ import {
   IonContent,
   IonButton,
   IonCard,
-  IonCardHeader, IonCardTitle, IonList, ModalController, IonItem
+  IonCardHeader, IonCardTitle, IonList, ModalController, IonItem, IonModal, IonButtons, IonCheckbox, IonInput
 } from '@ionic/angular/standalone';
-import {NameInputModalComponent} from "./name-input-modal.component";
-import {PermissionsModalComponent} from "./permissions-modal.component";
 
 import {ScavangerHuntManagerService} from "../services/scavanger-hunt-manager.service";
+
+import { Geolocation } from '@capacitor/geolocation';
+import { Camera } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonList, IonItem],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonCardHeader, IonCardTitle, IonList, IonItem, IonModal, IonButtons, IonCheckbox, IonInput],
 })
 export class HomePage {
+  playerName: string = '';
 
-  constructor(
-    private modalCtrl: ModalController,
-    private scavangerHunt: ScavangerHuntManagerService,
-    ) {}
+  cameraPermission: boolean = false;
+  locationPermission: boolean = false;
 
-  async startNewRun() {
-    const nameModal = await this.modalCtrl.create({
-      component: NameInputModalComponent,
-    });
-    await nameModal.present();
+  @ViewChild('nameModal', { static: false }) nameModal!: IonModal;
+  @ViewChild('permissionModal', { static: false }) permissionModal!: IonModal;
+  @ViewChild('playerNameInput', { static: false }) playerNameInput!: IonInput;
 
-    const { data } = await nameModal.onDidDismiss();
-    if (data?.name) {
-      const permissionsModal = await this.modalCtrl.create({
-        component: PermissionsModalComponent,
-      });
-      await permissionsModal.present();
-      this.scavangerHunt.startHunt(data?.name);
+  constructor() {}
+
+  onNameInput(event: any) {
+    this.playerName = event.target.value;
+  }
+
+  async openPermissionModal() {
+    if (this.playerName && this.permissionModal) {
+      await this.nameModal.dismiss();
+      await this.permissionModal.present();
     }
+  }
+
+  async onCameraPermissionChange(event: any) {
+    if (event.detail.checked) {
+      try {
+        await Camera.requestPermissions();
+        this.cameraPermission = true;
+      } catch (error) {
+        this.cameraPermission = false;
+      }
+    } else {
+      this.cameraPermission = false;
+    }
+  }
+
+  async onLocationPermissionChange(event: any) {
+    if (event.detail.checked) {
+      try {
+        await Geolocation.requestPermissions();
+        this.locationPermission = true;
+      } catch (error) {
+        this.locationPermission = false;
+      }
+    } else {
+      this.locationPermission = false;
+    }
+  }
+
+  canStart(): boolean {
+    return this.cameraPermission && this.locationPermission;
   }
 }
