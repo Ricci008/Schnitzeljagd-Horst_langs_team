218631adacce39aeea6ada07a0aa5cef3a18b06e
utigernils <utigernils@gmail.com>
2025-06-04 22:45:34 +0200

    feat; implement finished page

--- Schnitzeljagd/src/app/finished/finished.page.ts
+++ Schnitzeljagd/src/app/finished/finished.page.ts
@@ -1,24 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {IonContent, IonFooter} from '@ionic/angular/standalone';
+import {IonContent, IonFooter, IonIcon, IonTabBar, IonTabButton} from '@ionic/angular/standalone';
 import {ObjectiveStateComponent} from "../objective-state/objective-state.component";
 import {ObjectiveTitleComponent} from "../objective-title/objective-title.component";
 import {ProgressbarComponent} from "../progressbar/progressbar.component";
-import {ToolbarComponent} from "../toolbar/toolbar.component";
+import {ScavangerHuntManagerService} from "../services/scavanger-hunt-manager.service";
+import { ScavangerHuntDataService} from "../services/scavanger-hunt-data.service";
+import {addIcons} from "ionicons";
+import {exitOutline} from "ionicons/icons";
 
 @Component({
   selector: 'app-finished',
   templateUrl: './finished.page.html',
   styleUrls: ['./finished.page.scss'],
   standalone: true,
-    imports: [IonContent, CommonModule, FormsModule, IonFooter, ObjectiveStateComponent, ObjectiveTitleComponent, ProgressbarComponent, ToolbarComponent]
+  imports: [IonContent, CommonModule, FormsModule, IonFooter, ObjectiveStateComponent, ObjectiveTitleComponent, ProgressbarComponent, IonIcon, IonTabBar, IonTabButton]
 })
 export class FinishedPage implements OnInit {
+  points: number | undefined = 0;
+  skipedTasks: number | undefined = 0;
+  usedTime: number | undefined = 0;
 
-  constructor() { }
+  constructor(private scavengerHunt : ScavangerHuntManagerService, private scavengerHuntData : ScavangerHuntDataService, private router:Router) { }
 
   ngOnInit() {
+    addIcons({exitOutline});
+    const currentHuntId = this.scavengerHunt.getCurrentHuntId();
+    const currentScavengerHunt = this.scavengerHuntData.getHuntById(currentHuntId)
+    this.points = currentScavengerHunt?.points;
+    this.skipedTasks = currentScavengerHunt?.reductions;
+    this.usedTime = currentScavengerHunt?.totalTime;
+  }
+
+  onExit() {
+    this.router.navigate(["/tabs/leaderboard"]);
   }
 
 }
