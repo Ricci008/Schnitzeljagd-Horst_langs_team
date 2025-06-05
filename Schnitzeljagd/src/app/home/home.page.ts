036132c55e6637ceb184a433efbc09719fd65a9d
Nico Marcuard <Nico_Marcuard@sluz.ch>
2025-06-05 09:20:18 +0200

    feat: update top 3

--- Schnitzeljagd/src/app/home/home.page.ts
+++ Schnitzeljagd/src/app/home/home.page.ts
@@ -55,7 +55,7 @@ export class HomePage implements OnInit {
   }
 
   get topHunts(): ScavengerHunt[] {
-    return [...this.hunts].sort((a, b) => b.totalTime - a.totalTime);
+    return [...this.hunts].sort((a, b) => a.points - b.points);
   }
 
   get recentHunts(): ScavengerHunt[] {
